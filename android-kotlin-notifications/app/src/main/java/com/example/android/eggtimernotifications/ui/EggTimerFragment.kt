81ea7bb9664fa5544e1e8c18d081bc8ecac2b0a0
Tom Keane <tomkeane07@gmail.com>
2020-06-29 11:07:23 +0100

    FCM cloud messaging (single device & Topic)

--- android-kotlin-notifications/app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ android-kotlin-notifications/app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -61,6 +61,11 @@ class EggTimerFragment : Fragment() {
             getString(R.string.breakfast_notification_channel_name)
         )
 
+        // TODO: Step 3.4 call subscribe topics on start
+        subscribeTopic()
+
+        return binding.root
+
         return binding.root
     }
 
@@ -90,6 +95,22 @@ class EggTimerFragment : Fragment() {
         // TODO: Step 1.6 END create a channel
 
     }
+
+    // TODO: Step 3.3 subscribe to breakfast topic
+    private fun subscribeTopic() {
+        // [START subscribe_topics]
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+            .addOnCompleteListener { task ->
+                var msg = getString(R.string.message_subscribed)
+                if (!task.isSuccessful) {
+                    msg = getString(R.string.message_subscribe_failed)
+                }
+                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+            }
+        // [END subscribe_topics]
+    }
+
+
 
     companion object {
         fun newInstance() = EggTimerFragment()

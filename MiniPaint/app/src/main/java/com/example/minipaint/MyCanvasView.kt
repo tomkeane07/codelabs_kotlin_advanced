02bbd940753ed212a413b0adc96e797642670680
Tom Keane <tomkeane07@gmail.com>
2020-06-30 17:09:05 +0100

    Respond to motion on the display

--- MiniPaint/app/src/main/java/com/example/minipaint/MyCanvasView.kt
+++ MiniPaint/app/src/main/java/com/example/minipaint/MyCanvasView.kt
@@ -4,17 +4,23 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 
 private const val STROKE_WIDTH = 12f // has to be float
 
 class MyCanvasView(context: Context) : View(context) {
-    
+
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
     private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private var path = Path()
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+    
 
     // Set up the paint with which to draw.
     private val paint = Paint().apply {
@@ -42,6 +48,22 @@ class MyCanvasView(context: Context) : View(context) {
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
     }
 
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
 
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
+    }
+
+    private fun touchStart() {}
+
+    private fun touchMove() {}
+
+    private fun touchUp() {}
 
 }

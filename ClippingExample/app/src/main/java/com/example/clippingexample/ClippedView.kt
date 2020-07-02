b192106fd9b25f419fb5027070a4108338107073
Tom Keane <tomkeane07@gmail.com>
2020-07-02 14:07:54 +0100

    Create a method to draw the shapes

--- ClippingExample/app/src/main/java/com/example/clippingexample/ClippedView.kt
+++ ClippingExample/app/src/main/java/com/example/clippingexample/ClippedView.kt
@@ -1,6 +1,8 @@
 package com.example.clippingexample
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
@@ -25,6 +27,7 @@ class ClippedView @JvmOverloads constructor(
     private val clipRectLeft = resources.getDimension(R.dimen.clipRectLeft)
     private val rectInset = resources.getDimension(R.dimen.rectInset)
     private val smallRectOffset = resources.getDimension(R.dimen.smallRectOffset)
+    private val circleRadius = resources.getDimension(R.dimen.circleRadius)
     private val textOffset = resources.getDimension(R.dimen.textOffset)
     private val textSize = resources.getDimension(R.dimen.textSize)
     //Set up the coordinates for two columns
@@ -37,5 +40,74 @@ class ClippedView @JvmOverloads constructor(
     private val rowFour = rowThree + rectInset + clipRectBottom
     private val textRow = rowFour + (1.5f * clipRectBottom)
 
-    
+
+
+    override fun onDraw(canvas: Canvas){
+        super.onDraw(canvas)
+        drawBackAndUnclippedRectangle(canvas)
+        drawDifferenceClippingExample(canvas)
+        drawCircularClippingExample(canvas)
+        drawIntersectionClippingExample(canvas)
+        drawCombinedClippingExample(canvas)
+        drawRoundedRectangleClippingExample(canvas)
+        drawOutsideClippingExample(canvas)
+        drawSkewedTextExample(canvas)
+        drawTranslatedTextExample(canvas)
+        // drawQuickRejectExample(canvas)
+    }
+
+    private fun drawBackAndUnclippedRectangle(canvas: Canvas){
+        canvas.drawColor(Color.GRAY)
+        canvas.save()
+        canvas.translate(columnOne,rowOne)
+        drawClippedRectangle(canvas)
+        canvas.restore()
+    }
+    private fun drawDifferenceClippingExample(canvas: Canvas){
+    }
+    private fun drawCircularClippingExample(canvas: Canvas){
+    }
+    private fun drawIntersectionClippingExample(canvas: Canvas){
+    }
+    private fun drawCombinedClippingExample(canvas: Canvas){
+    }
+    private fun drawRoundedRectangleClippingExample(canvas: Canvas){
+    }
+    private fun drawOutsideClippingExample(canvas: Canvas){
+    }
+    private fun drawTranslatedTextExample(canvas: Canvas){
+    }
+    private fun drawSkewedTextExample(canvas: Canvas){
+    }
+    private fun drawQuickRejectExample(canvas: Canvas){
+    }
+    private fun drawClippedRectangle(canvas: Canvas) {
+        canvas.clipRect(
+            clipRectLeft,clipRectTop,
+            clipRectRight,clipRectBottom
+        )
+
+        canvas.drawColor(Color.WHITE)
+        paint.color = Color.RED
+        canvas.drawLine(
+            clipRectLeft,clipRectTop,
+            clipRectRight,clipRectBottom,paint
+        )
+
+        paint.color = Color.GREEN
+        canvas.drawCircle(
+            circleRadius,clipRectBottom - circleRadius,
+            circleRadius,paint
+        )
+
+        paint.color = Color.BLUE
+// Align the RIGHT side of the text with the origin.
+        paint.textSize = textSize
+        paint.textAlign = Paint.Align.RIGHT
+        canvas.drawText(
+            context.getString(R.string.clipping),
+            clipRectRight,textOffset,paint
+        )
+
+    }
 }

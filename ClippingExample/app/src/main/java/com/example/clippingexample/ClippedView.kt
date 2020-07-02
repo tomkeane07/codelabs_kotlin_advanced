a631a04dde712f11c83d5d8bdff8e8bcfb1589ad
Tom Keane <tomkeane07@gmail.com>
2020-07-02 17:05:02 +0100

    Implement drawRoundedRectangleClippingExample(canvas)

--- ClippingExample/app/src/main/java/com/example/clippingexample/ClippedView.kt
+++ ClippingExample/app/src/main/java/com/example/clippingexample/ClippedView.kt
@@ -40,6 +40,13 @@ class ClippedView @JvmOverloads constructor(
     private val rowFour = rowThree + rectInset + clipRectBottom
     private val textRow = rowFour + (1.5f * clipRectBottom)
 
+    private var rectF = RectF(
+        rectInset,
+        rectInset,
+        clipRectRight - rectInset,
+        clipRectBottom - rectInset
+    )
+
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
@@ -169,6 +176,17 @@ class ClippedView @JvmOverloads constructor(
     }
 
     private fun drawRoundedRectangleClippingExample(canvas: Canvas) {
+        canvas.save()
+        canvas.translate(columnTwo,rowThree)
+        path.rewind()
+        path.addRoundRect(
+            rectF,clipRectRight / 4,
+            clipRectRight / 4, Path.Direction.CCW
+        )
+        // CCW - counter clockwise
+        canvas.clipPath(path)
+        drawClippedRectangle(canvas)
+        canvas.restore()
     }
 
     private fun drawOutsideClippingExample(canvas: Canvas) {
